6914ba98312a15a8476a742b0e89cacf3c822aa0
salmaQueen <salmawasee9@gmail.com>
2023-09-26 20:05:30 +0300

    tested

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,9 +28,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
+				print_array(array, size);
 			}
-			
-			print_array(array, size);
 		}
 
 		if (swapped == 0)
