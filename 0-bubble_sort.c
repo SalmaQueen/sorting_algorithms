cf6f6937b209ff6e8b726a5fadb74fb9fadf1c6b
salmaQueen <salmawasee9@gmail.com>
2023-09-26 20:20:45 +0300

    again and again

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,8 +27,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
 
