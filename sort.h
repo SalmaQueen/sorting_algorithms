71f38a72e699c2b0dfb74fa77c6ff8265b951404
salmaQueen <salmawasee9@gmail.com>
2023-09-26 21:54:54 +0300

    insert sort

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#include <stddef.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,4 +18,4 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
